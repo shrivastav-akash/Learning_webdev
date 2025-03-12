67d3556a69212a18d61d45f6896e551a459cd9ef
akash shrivastav <shrivastav.work@gmail.com>
2025-03-12 17:38:39 +0530

    updated index.js

--- expressDir/index.js
+++ expressDir/index.js
@@ -6,7 +6,14 @@ app.listen(port, () => {
     console.log(`app is listening at port: ${port}`);
 });
 
-app.send((req, res), () => {
-    console.log("req received");
-    res.send("this is a basic responce");
-})
+app.get("/", (req, res) => {
+	res.send("you contacted root path");
+});
+
+app.get("/apple", (req, res) => {
+	res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+	res.send("you contacted orange path");
+});
