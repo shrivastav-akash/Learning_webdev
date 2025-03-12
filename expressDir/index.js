636bd6560017ed23c918ecc2bcff0aeea6f7d965
akash shrivastav <shrivastav.work@gmail.com>
2025-03-12 08:36:57 +0530

    using app.use

--- expressDir/index.js
+++ expressDir/index.js
@@ -5,3 +5,9 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening at port: ${port}`);
 });
+
+app.use((req, res), () => {
+    console.log("request recieved");
+    res.send("this is a basic response.....!");
+    
+});
